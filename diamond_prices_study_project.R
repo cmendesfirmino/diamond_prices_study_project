f6393748b2c5916c13f05bb02342aed72dcd0fad
Cristian Firmino <cmendesfirmino@gmail.com>
2020-06-07 23:03:34 -0300

    comming...

--- diamond_prices_study_project.R
+++ diamond_prices_study_project.R
@@ -1,2 +1,12 @@
 train_df <- read.csv('diamonds.csv')
 
+linearMod <- lm(price ~ carat + cut + color + clarity, data=train_df)
+summary(linearMod)
+head(train_df)
+
+test_df <- read.csv('new-diamonds.csv')
+p <- predict(linearMod, test_df)
+head(p)
+total <- sum(p)
+result <- total * 0.7
+print(result)
